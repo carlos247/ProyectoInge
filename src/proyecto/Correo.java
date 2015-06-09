8853374f9a32ba1bd6136f94d81127e2e399503a
carlos arronte <carloslazaro247@yahoo.com>
2015-06-09 10:56:36 -0600

    Envia y recive correos

--- src/proyecto/Correo.java
+++ src/proyecto/Correo.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Properties;
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
@@ -31,9 +32,9 @@ public class Correo {
     static private Properties p;
     static private Session s;
     static private Transport t;
-   
+
     public static void conexion() {
-      
+
         p = new Properties(); // inicio de sesion en el correo y mando las propiedades necesarias para enviar el correo
         p.put("mail.smtp.host", "smtp.gmail.com");
         p.setProperty("mail.smtp.starttls.enable", "true");
@@ -45,12 +46,18 @@ public class Correo {
     public static void iniciarSesion() {
         s = Session.getDefaultInstance(p, null); // aqui inicio sesion para poder habilitar el usuario
         Base_de_datos BD = new Base_de_datos();
-        String destino = BD.dat();
-        String asunto = "funciona";
-        String mensajeTexto = "al fin";
-        String archivo = "formulario.html";
-        System.out.println(destino + "/" + asunto + "/" + mensajeTexto + "/" + archivo + "\n");
-        EnviarCorreo(destino, asunto, mensajeTexto, archivo);
+        ArrayList<String> destinoA = BD.dat();
+        String destino = "";
+        for (String s : destinoA) {
+            destino += s;
+            String asunto = "funciona";
+            String mensajeTexto = "al fin";
+            String archivo = "formulario.html";
+            System.out.println(destino + "/" + asunto + "/" + mensajeTexto + "/" + archivo + "\n");
+            EnviarCorreo(destino, asunto, mensajeTexto, archivo);
+            destino = " ";
+        }
+
     }
 
     public static void EnviarCorreo(String destino, String asunto, String mensajeTexto, String Archivo) {
@@ -84,10 +91,6 @@ public class Correo {
             System.out.printf("Correo enviado con exito");
         } catch (Exception e) {
             System.out.printf("Error al enviar correo, no me puedo autentificar: " + e + " " + destino + " " + mensajeTexto + " " + asunto + " " + Archivo);
-          
         }
     }
-
-
-
 }
